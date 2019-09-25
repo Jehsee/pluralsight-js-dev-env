47f21515e3613ebce3e784c640d8eafbdca2aed8
Jehsee <jessehwang86@yahoo.com>
2019-09-24 22:21:19 -0700

    final comit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -3,6 +3,7 @@ import path from 'path';
 import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+import { getMaxListeners } from 'cluster';
 
 const port = 3000;
 const app = express();
@@ -15,6 +16,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  // hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id":1,"firstName":"Taylor", "lastName":"Smith","email":"taylor@gmail.com"},
+    {"id":2,"firstName":"Tammy", "lastName":"Norton","email":"tammy@gmail.com"},
+    {"id":3,"firstName":"Tina", "lastName":"Lee","email":"tina@gmail.com"}
+  ])
 });
 
 app.listen(port, function(err) {
